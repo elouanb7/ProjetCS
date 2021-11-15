d1be792b1d385871c4bfbce28b080e530e6bcbb4
elouanb7 <elouanb7@gmail.com>
2021-11-15 23:51:28 +0100

    ajout adherent

---
+++ Raminagrobis.DAL/Adherent_DAL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raminagrobis.DAL
+{
+    class Adherent_DAL
+    {
+        public int ID { get; private set; }
+        public string Societe { get; set; }
+        public bool Civilite { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Mail { get; set; }
+        public string Adresse { get; set; }
+        //ajouter la date
+        public bool Desactive { get; set; }
+
+        public Adherent_DAL(string societe, bool civilite, string nom, string prenom, string mail, string adresse, bool desactive)
+            => (Societe, Civilite, Nom, Prenom, Mail, Adresse, Desactive) = (societe, civilite, nom, prenom, mail, adresse, desactive);
+
+        public Adherent_DAL(int id, string societe, bool civilite, string nom, string prenom, string mail, string adresse, bool desactive)
+            => (ID, Societe, Civilite, Nom, Prenom, Mail, Adresse, Desactive) = (id, societe, civilite, nom, prenom, mail, adresse, desactive);
+
+    }
+}
---
+++ raminagrobis/Adherent.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raminagrobis
+{
+    class Adherent
+    {
+        #region champs et accesseurs
+
+        /// <summary>
+        /// ID de l'adherent
+        /// </summary>
+        public int ID { get; private set; }
+
+        /// <summary>
+        /// Societe de l'adherent
+        /// </summary>
+        public string Societe { get; set; }
+
+        /// <summary>
+        /// Civilite du contact chez l'adherent (0 = Femme / 1 = Homme)
+        /// </summary>
+        public bool Civilite { get; set; }
+
+        /// <summary>
+        /// Nom du contact chez l'adherent
+        /// </summary>
+        public string Nom { get; set; }
+
+        /// <summary>
+        /// Prenom du contact chez l'adherent
+        /// </summary>
+        public string Prenom { get; set; }
+
+        /// <summary>
+        /// Mail de l'adherent
+        /// </summary>
+        public string Mail { get; set; }
+
+        /// <summary>
+        /// Adresse de l'adherent
+        /// </summary>
+        public string Adresse { get; set; }
+
+        /// <summary>
+        /// Booléen informant de l'état de l'adherent (0 = Actif / 1 = Desactivé)
+        /// </summary>
+        public bool Desactive { get; set; }
+
+        #endregion
+
+        #region Constructeur
+
+        /// <summary>
+        /// Construit un adherent
+        /// </summary>
+        /// <param name="id">ID de l'adherent (ID)</param>
+        /// <param name="societe">Societe de l'adherent (Societe)</param>
+        /// <param name="civilite">Civilite du contact chez l'adherent (Civilite)</param>
+        /// <param name="nom">Nom du contact chez l'adherent (Nom)</param>
+        /// <param name="prenom">Prenom du contact chez l'adherent (Prenom)</param>
+        /// <param name="mail">Mail de l'adherent (Mail)</param>
+        /// <param name="adresse">Adresse de l'adherent (Adresse)</param>
+        /// <param name="desactive">Booléen informant de l'état de l'adherent (Desactive)</param>
+        public Adherent(int id, string societe, bool civilite, string nom, string prenom, string mail, string adresse, bool desactive)
+        => (ID, Societe, Civilite, Nom, Prenom, Mail, Adresse, Desactive) = (id, societe, civilite, nom, prenom, mail, adresse, desactive);
+
+        #endregion
+
+        #region Méthodes
+
+        public override string ToString()
+        {
+            return $"({ID}; {Societe}; {Civilite}; {Nom}; {Prenom}; {Mail}; {Adresse}; {Desactive})";
+        }
+        #endregion
+
+    }
+}
