4585be051e53ef94fee8d0b809f80c1b9f07b378
Romain <romain.caille@epsi.com>
2021-11-15 11:56:47 +0100

    premiere version

---
+++ Raminagrobis.DAL/Fournisseur_DAL.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raminagrobis.DAL
+{
+    public class Fournisseur_DAL
+    {
+        public int ID { get; private set; }
+        public string Societe { get; set; }
+        public bool Civilite { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Mail { get; set; }
+        public string Adresse { get; set; }
+        public bool Desactive { get; set; }
+
+        public Fournisseur_DAL(string societe, bool civilite, string nom, string prenom, string mail, string adresse, bool desactive)
+            => (Societe, Civilite, Nom, Prenom, Mail, Adresse, Desactive) = (societe, civilite, nom, prenom, mail, adresse, desactive);
+
+        public Fournisseur_DAL(int id, string societe, bool civilite, string nom, string prenom, string mail, string adresse, bool desactive)
+            => (ID, Societe, Civilite, Nom, Prenom, Mail, Adresse, Desactive) = (id, societe, civilite, nom, prenom, mail, adresse, desactive);
+
+    }
+}
